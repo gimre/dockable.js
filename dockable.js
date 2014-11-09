bab7e11d7cba171df788a2f4398d821665a5534b
gimre <imregabriel@gmail.com>
2014-11-09 23:31:25 +0200

    added docked status

--- dockable.js
+++ dockable.js
@@ -1,116 +1,3 @@
-// ( function ( w, d ) {
-//     w.onload = function ( ) {
-
-//         var model = {
-//             dragging: null,
-//             token: ( function ( ) {
-//                 var token = d.createElement( 'div' );
-//                 token.className = 'dockable-token';
-//                 d.body.appendChild( token );
-//                 return token;
-//             } )( ),
-//             dockZones: ( function ( ) {
-//                 return [ 'top', 'right', 'bottom', 'left' ].map( function ( zone ) {
-//                     var elem = d.createElement( 'div' );
-//                     elem.className = [ 'dockable-dropzone dockable-transitioned', zone ].join( ' ' );
-//                     d.body.insertBefore( elem, d.body.firstChild );
-//                     return elem;
-//                 } );
-//             } )( ),
-//             dockables: d.querySelectorAll( '[dockable]' )
-//         };
-
-//         var throttle = function ( fn, delay ) {
-//             var lock = false;
-
-//             return function ( ) {
-//                 var args = arguments;
-
-//                 if ( !lock ) {
-//                     lock = true;
-//                     fn.apply( fn, args );
-
-//                     setTimeout( function () {
-//                         lock = false;
-//                     }, delay );
-//                 }
-//             }
-//         };
-
-//         var moveElement = function ( element, x, y ) {
-//             transformElement( model.token, 'translate3d(' + x + 'px,' + y + 'px,' + 0 + ')' );
-//         };
-
-//         var moveToMouseEvent = function ( element, mouseEvent ) {
-//             var x = mouseEvent.clientX + d.body.scrollLeft,
-//                 y = mouseEvent.clientY + d.body.scrollTop;
-//             return moveElement( element, x, y );
-//         };
-
-//         var transformElement = function ( element, value ) {
-//             element.style.webkitTransform =
-//             element.style.MozTransform =
-//             element.style.transform = value;
-//         };
-
-//         var dragOnce = function ( e ) {
-//             moveToMouseEvent( model.token, e );
-//         };
-
-//         var dragElement = dragOnce; //throttle( dragOnce, 250 );
-
-//         var dragStart = function ( event ) {
-//             model.dragging = event.target;
-//             model.token.classList.add( 'dragging' );
-
-//             model.dockZones.forEach( function ( zone ) {
-//                 zone.classList.add( 'visible' );
-//             } );
-
-//             dragOnce( event );
-
-//             w.addEventListener( 'mousemove', dragElement );
-//         };
-
-//         var dragEnd = function ( event ) {
-//             if ( model.dragging ) {
-//                 w.removeEventListener( 'mousemove', dragElement );
-
-//                 dragOnce( event );
-
-//                 model.dockZones.forEach( function ( zone ) {
-//                     zone.classList.remove( 'visible' );
-//                 } );
-
-//                 model.token.classList.remove( 'dragging' );
-//                 model.dragging = null;
-//                 moveElement( model.token, 0, 0 );
-//             }
-//         };
-
-//         var dockDragged = function ( event ) {
-//             if ( model.dragging ) {
-//                 model.dragging.classList.add( 'docked' );
-//                 event.target.appendChild( model.dragging );
-//             }
-//         };
-        
-        
-//         // hook-up
-
-//         [].forEach.call( model.dockZones, function ( zone ) {
-//             zone.addEventListener( 'mouseup', dockDragged );
-//         } );
-
-//         [].forEach.call( model.dockables, function ( elem ) {
-//             elem.addEventListener( 'mousedown', dragStart );
-//         } ) ;
-
-//         w.addEventListener( 'mouseup', dragEnd );
-//     };
-
-// } )( window, document );
-
 ( function ( w, d ) {
 
     w.onload = function ( ) {
@@ -131,6 +18,7 @@
                 ].map( function ( handle ) {
                     var elem = d.createElement( 'div' );
                     elem.className = [ 'dockhandle', handle ].join( ' ' );
+                    elem.dockableClassName = handle;
                     d.body.appendChild( elem );
                     return elem;
                 } );
@@ -195,6 +83,11 @@
             var target = event.target;
 
             if ( is( target, 'dockable' ) ) {
+                if ( is( target, 'docked' ) ) {
+                    removeElementClass( target, 'docked' );
+                    removeElementClass( target, target.dockedClassName );
+                }
+
                 model.dragging = {
                     target:  target,
                     offsetX: getOffset( target, 'offsetLeft' ),
@@ -227,6 +120,9 @@
             if ( model.dragging ) {
                 if ( is( target, 'dockhandle' ) ) {
                     target.parentNode.appendChild( model.dragging.target );
+                    addElementClass( model.dragging.target, 'docked' );
+                    addElementClass( model.dragging.target, target.dockableClassName );
+                    model.dragging.target.dockedClassName = target.dockableClassName;
                 }
                 moveHandlesToElement( null, true );
                 moveElement( model.dragging.target, 0, 0 );
